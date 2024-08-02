fb8c63d85637da41c875597aa83f7633b49c1d34
Eduardo-Lisboa <henriqueeduardo63@gmail.com>
2024-08-01 23:43:29 -0300

    chore: Refactor FizzBuzz module to handle file read errors

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -7,11 +7,25 @@ defmodule FizzBuzz do
 
   end
 
-  def handle_file_read({:ok, result}) do
-    result
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_number/1)
+    {:ok, result}
   end
-  def handle_file_read({:error, reason}), do: "Error reading the file #{reason}"
 
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file #{reason}"}
+
+  defp convert_and_evaluate_number(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_number()
+
+  end
+
+  defp evaluate_number(number) when rem(number,3) == 0 and rem(number,5) == 0, do: "FizzBuzz"
+  defp evaluate_number(number) when rem(number,3) == 0, do: "Fizz"
+  defp evaluate_number(number) when rem(number,5) == 0, do: "Buzz"
+  defp evaluate_number(number), do: number
 end
